a8cfacc910092f980ff813579b5c9b357283a1eb
vlasvlasvlas <vbellini@mail.austral.edu.ar>
2022-12-22 17:22:25 -0300

    vercel

--- types/react-i18next.d.ts
+++ types/react-i18next.d.ts
@@ -3,6 +3,6 @@ import { defaultNS, resources } from '../src/i18n';
 declare module 'react-i18next' {
   interface CustomTypeOptions {
     defaultNS: typeof defaultNS;
-    resources: typeof resources['cn'];
+    resources: typeof resources['es'];
   }
 }
