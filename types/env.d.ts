53da0e3ede2b82eafa3986c91a6f6428b3949681
vlasvlasvlas <vbellini@mail.austral.edu.ar>
2022-12-22 15:21:29 -0300

    copy

---
+++ types/env.d.ts
@@ -0,0 +1,11 @@
+interface ImportMetaEnv {
+  VITE_APP_TITLE: string;
+  VITE_GITHUB_URL: string;
+  VITE_GITHUB_ACCESS_TOKEN: string;
+  VITE_GITHUB_OWNER: string;
+  VITE_GITHUB_REPO: string;
+  VITE_GITHUB_MILESTONE_POSTS: string;
+  VITE_GITHUB_MILESTONE_SNIPPETS: string;
+  VITE_ARTICLE_PAGE_SIZE: string;
+  VITE_COMMENT_PAGE_SIZE: string;
+}
